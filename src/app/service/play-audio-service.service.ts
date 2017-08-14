3119744b847eaf6768ec1e1fb06e0b2bf751ec6c
xiaoCong2014 <1149023208@qq.com>
2017-08-14 23:01:07 +0800

    服务里引入组件 失败 . 组件里引入服务 OK

--- src/app/service/play-audio-service.service.ts
+++ src/app/service/play-audio-service.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {AudioPlayerComponent} from '../audio-player/audio-player.component';
 
 /**
  * 播放音频服务
@@ -6,6 +7,18 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class PlayAudioServiceService {
 
-  constructor() { }
+  // @Injectable
+  // a33udioPlayerComponent: AudioPlayerComponent;
 
+  // constructor( private audioPlayerComponent: AudioPlayerComponent ) { }// todo 服务里引入组件 失败 . 组件里引入服务 OK
+
+  constructor(  ) { }//
+
+  playAudio(): void {
+    // this.audioPlayerComponent.playAudio();
+
+    // myAudio.play();
+
+    console.log("PlayAudioServiceService playAudio()");
+  }
 }
