bbcfbf8c59a5145708592e0144870ae94a3f9f06
xiaoCong2014 <1149023208@qq.com>
2017-08-15 09:28:01 +0800

    设置子组件的属性

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title : string = 'app';
+  // appTitle : string = 'app';
+
+  title: string = 'Lenka.jpg';
+  imgSrc: string = '../assets/image/Lenka.jpg';
 }
